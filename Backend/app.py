5733d27318e8cb9610664952a03e71b6e0a7de00
arpitbytes507 <arpit.dhumane@gmail.com>
2025-07-19 14:52:57 +0530

    Flask API

--- Backend/app.py
+++ Backend/app.py
@@ -1,23 +1,27 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask import Flask, send_from_directory, jsonify, request
 import joblib
 import pandas as pd
+import os
 
-# Load your trained ML model
-model=joblib.load("salary_model.pkl")
+app = Flask(__name__, static_folder="static", static_url_path="")
 
-app=Flask(__name__)
-CORS(app)  # Allow requests from frontend
+# Load ML model
+model = joblib.load("model.pkl")
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    data = request.get_json()
-    print("Received data:", data)
-    # Convert JSON to DataFrame
+    data = request.json
     df = pd.DataFrame([data])
-    # Predict salary
     prediction = model.predict(df)[0]
     return jsonify({"predicted_salary": round(prediction, 2)})
 
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def serve_react(path):
+    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, "index.html")
+
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True)
